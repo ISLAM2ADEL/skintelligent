aa3a82652565ae1674b93a2fc27d4fdb73322d6b
= <tarekmohammed022@gmail.com>
2025-01-30 22:22:10 +0200

    Add login screen

---
+++ lib/Features/Onboarding/Screens_onboarding/Widgets_onboarding/onBoardingPage.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/cupertino.dart';
+//
+// class OnBoardingPage extends StatelessWidget {
+//   const OnBoardingPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return PageView(
+//       children: [
+//         Image.asset()
+//       ],
+//     )
+//   }
+// }
---
+++ lib/Widgets/customTextField.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Customtextfield extends StatelessWidget {
+  Customtextfield(
+      {required this.hintM,
+      required this.MyController,
+      required this.validate,
+      required this.myIcon,
+      this.sufIcon});
+  // Attributes
+  TextEditingController MyController;
+  String? hintM;
+  IconData myIcon;
+  IconData? sufIcon;
+  String? Function(String?)? validate;
+  //---------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: MyController,
+      validator: validate,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20)  ,
+        ),
+          prefixIcon: Icon(myIcon , color: Colors.grey,),
+          hintText: hintM,
+          hintStyle: const TextStyle(fontSize: 14, color: Colors.grey)),
+    );
+  }
+}
