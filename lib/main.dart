4ec133c15e477433b84074a65c898409c6a02701
= <tarekmohammed022@gmail.com>
2025-04-18 17:03:19 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:skintelligent/controllers/api/dio_consumer.dart';
 import 'package:skintelligent/controllers/repositories/user_repository.dart';
+import 'package:skintelligent/cubit/doctor_cubit/doctor_cubit.dart';
 import 'package:skintelligent/cubit/user_cubit/user_cubit.dart';
 import 'package:skintelligent/screens/otp/otp_screen.dart';
 import 'package:skintelligent/test_space/appoinment.dart';
@@ -45,6 +46,11 @@ class MyApp extends StatelessWidget {
           create: (context) =>
               UserCubit(UserRepository(api: DioConsumer(dio: Dio()))),
         ),
+        BlocProvider(
+          create: (context) => DoctorCubit(
+            UserRepository(api: DioConsumer(dio: Dio())),
+          ),
+        )
       ],
       child: GetMaterialApp(
         routes: {
