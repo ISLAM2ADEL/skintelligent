ad462ccaaf6cdc270bb9e1169d243afc031425bf
eslam-1-adel <eslamadel3052002@gmail.com>
2025-04-18 19:57:48 +0200

    appointment doctors image

--- lib/screens/appointment/appointment.dart
+++ lib/screens/appointment/appointment.dart
@@ -103,7 +103,8 @@ class Appointment extends StatelessWidget {
         child: Row(
           children: [
             CircleAvatar(
-              backgroundImage: NetworkImage(profilePicture),
+              //backgroundImage: NetworkImage(profilePicture),
+              backgroundImage: NetworkImage("https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "),
               radius: 45,
             ),
             const SizedBox(width: 7),
