932976b6c39140376fa52ea9f9d6a4b1b8a97484
= <tarekmohammed022@gmail.com>
2025-04-18 14:59:34 +0200

    add doctorById endpoint

--- lib/controllers/api/endpoint.dart
+++ lib/controllers/api/endpoint.dart
@@ -4,22 +4,42 @@ class Endpoint {
   // static String signIn = "user/signin";
   static String signIn = "/api/auth/login";
   static String signUp = "user/signup";
+static String doctorById(String id) => "/api/doctors/$id";
   static String getUserDataEndPoint(id) {
     return "user/get-user/$id";
   }
 }
 
 class ApiKey {
-  static String status = "statusCode";
-  static String errorMessage = "message";
-  static String email = "email";
-  static String password = "password";
-  static String token = "token";
-  static String message = "message";
-  static String id = "id";
-  static String name = "name";
-  static String phone = "phone";
-  static String confirmPassword = "confirmPassword";
-  static String location = "location";
-  static String profilePic = "profilePic";
+  static const String status = "statusCode";
+  static const String errorMessage = "message";
+
+  // Auth-related
+  static const String email = "email";
+  static const String password = "password";
+  static const String confirmPassword = "confirmPassword";
+  static const String token = "token";
+
+  // Doctor/User info
+  static const String id = "id";
+  static const String name = "name"; // fallback if needed
+  static const String firstName = "firstName";
+  static const String lastName = "lastName";
+  static const String gender = "gender";
+  static const String dateOfBirth = "dateOfBirth";
+  static const String phone = "phoneNumber";
+  static const String licenseNumber = "licenseNumber";
+  static const String experienceYears = "experienceYears";
+  static const String defaultExaminationFee = "defaultExaminationFee";
+  static const String defaultConsultationFee = "defaultConsultationFee";
+  static const String profilePic = "profilePicture";
+  static const String aboutMe = "aboutMe";
+  static const String qualification = "qualification";
+  static const String isApproved = "isApproved";
+  static const String createdDate = "createdDate";
+  static const String updatedDate = "updatedDate";
+
+  // Location (if used later)
+  static const String location = "location";
 }
+
