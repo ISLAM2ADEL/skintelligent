e45eafadca419f49842cbe73afcd3d9d0082c1fa
eslam-1-adel <eslamadel3052002@gmail.com>
2025-04-28 22:08:29 +0300

    update appointment logic

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -2,6 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:skintelligent/commons.dart';
 final getIt = GetIt.instance;
 void setputServiceLocator() {
-  getIt.registerSingleton<CacheHelper>(CacheHelper()); 
+  getIt.registerSingleton<CacheHelper>(CacheHelper());
 }
 
