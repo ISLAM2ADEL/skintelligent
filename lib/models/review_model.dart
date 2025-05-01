1a98ef2b21dbdc14def3ae529bc10dd21f60ce6b
= <tarekmohammed022@gmail.com>
2025-05-01 06:00:47 +0300

    update review_model (change the Data Type )

--- lib/models/review_model.dart
+++ lib/models/review_model.dart
@@ -1,21 +1,23 @@
-import 'package:skintelligent/commons.dart';
+import 'package:skintelligent/controllers/api/endpoint.dart';
 
 class ReviewModel {
-  final String pageIndex;
-  final String pageSize;
+  final int pageIndex;
+  final int pageSize;
   final int count;
   final List<ReviewItem> reviews;
 
-  ReviewModel(
-      {required this.pageIndex,
-      required this.pageSize,
-      required this.count,
-      required this.reviews});
+  ReviewModel({
+    required this.pageIndex,
+    required this.pageSize,
+    required this.count,
+    required this.reviews,
+  });
+
   factory ReviewModel.fromJson(Map<String, dynamic> json) {
     return ReviewModel(
       pageIndex: json[ApiKey.pageIndex],
       pageSize: json[ApiKey.pageSize],
-      count: (json[ApiKey.count]),
+      count: json[ApiKey.count],
       reviews: (json[ApiKey.data] as List)
           .map((e) => ReviewItem.fromJson(e))
           .toList(),
@@ -31,18 +33,20 @@ class ReviewItem {
   final String patientImage;
   final String createdAt;
 
-  ReviewItem(
-      {required this.id,
-      required this.comment,
-      required this.rating,
-      required this.patientName,
-      required this.patientImage,
-      required this.createdAt});
+  ReviewItem({
+    required this.id,
+    required this.comment,
+    required this.rating,
+    required this.patientName,
+    required this.patientImage,
+    required this.createdAt,
+  });
+
   factory ReviewItem.fromJson(Map<String, dynamic> json) {
     return ReviewItem(
       id: json[ApiKey.id],
       comment: json[ApiKey.comment],
-      rating: (json[ApiKey.rating]),
+      rating: json[ApiKey.rating],
       patientName: json[ApiKey.patientName],
       patientImage: json[ApiKey.patientImage],
       createdAt: json[ApiKey.createdAt],
