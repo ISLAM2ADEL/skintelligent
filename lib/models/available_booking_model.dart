6dff19119e5802f0365b05758303b4f51a578911
= <tarekmohammed022@gmail.com>
2025-05-02 03:27:31 +0300

    update weekly sechdule model

--- lib/models/available_booking_model.dart
+++ lib/models/available_booking_model.dart
@@ -46,3 +46,67 @@ class AvailableBookingModel {
     return AvailableBookingModel(bookings: parsed);
   }
 }
+
+// ----------------------
+class Session {
+  final int id;
+  final String startTime;
+  final String endTime;
+  final bool isCanceled;
+  final bool isBooked;
+  final String repeatUntil;
+
+  Session({
+    required this.id,
+    required this.startTime,
+    required this.endTime,
+    required this.isCanceled,
+    required this.isBooked,
+    required this.repeatUntil,
+  });
+
+  factory Session.fromJson(Map<String, dynamic> json) {
+    return Session(
+      id: json['id'],
+      startTime: json['startTime'],
+      endTime: json['endTime'],
+      isCanceled: json['isCanceled'],
+      isBooked: json['isBooked'],
+      repeatUntil: json['repeatUntil'],
+    );
+  }
+}
+
+
+
+class WeeklySchedule {
+  final List<Session> sunday;
+  final List<Session> monday;
+  final List<Session> tuesday;
+  final List<Session> wednesday;
+  final List<Session> thursday;
+  final List<Session> friday;
+  final List<Session> saturday;
+
+  WeeklySchedule({
+    required this.sunday,
+    required this.monday,
+    required this.tuesday,
+    required this.wednesday,
+    required this.thursday,
+    required this.friday,
+    required this.saturday,
+  });
+
+  factory WeeklySchedule.fromJson(Map<String, dynamic> json) {
+    return WeeklySchedule(
+      sunday: (json['Sunday'] as List).map((e) => Session.fromJson(e)).toList(),
+      monday: (json['Monday'] as List).map((e) => Session.fromJson(e)).toList(),
+      tuesday: (json['Tuesday'] as List).map((e) => Session.fromJson(e)).toList(),
+      wednesday: (json['Wednesday'] as List).map((e) => Session.fromJson(e)).toList(),
+      thursday: (json['Thursday'] as List).map((e) => Session.fromJson(e)).toList(),
+      friday: (json['Friday'] as List).map((e) => Session.fromJson(e)).toList(),
+      saturday: (json['Saturday'] as List).map((e) => Session.fromJson(e)).toList(),
+    );
+  }
+}
