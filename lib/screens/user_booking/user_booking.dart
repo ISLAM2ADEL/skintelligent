bd2c891e51ba89f499429ecfa8c567aa7d9dc7a3
= <tarekmohammed022@gmail.com>
2025-05-02 03:35:46 +0300

    update user_booking screen and comment old code

--- lib/screens/user_booking/user_booking.dart
+++ lib/screens/user_booking/user_booking.dart
@@ -1,3 +1,53 @@
+// import 'package:skintelligent/commons.dart';
+// import 'package:skintelligent/models/available_booking_model.dart';
+// import 'package:skintelligent/utiles/booking_store.dart';
+// import 'package:intl/intl.dart';
+
+// class UserBookingScreen extends StatefulWidget {
+//   const UserBookingScreen({super.key});
+//   static const String id = 'UserBookingScreen';
+
+//   @override
+//   State<UserBookingScreen> createState() => _UserBookingScreenState();
+// }
+
+// class _UserBookingScreenState extends State<UserBookingScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<AppointmentSlot> bookedSlots =
+//         BookingStore().selectedSlots.toList();
+
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('My Bookings')),
+//       body: bookedSlots.isEmpty
+//           ? const Center(child: Text("No bookings yet."))
+//           : ListView.builder(
+//               itemCount: bookedSlots.length,
+//               itemBuilder: (context, index) {
+//                 final slot = bookedSlots[index];
+//                 final dayName = DateFormat.EEEE().format(slot.startTime);
+//                 final date = slot.startTime.toLocal().toString().split(' ')[0];
+//                 final time =
+//                     TimeOfDay.fromDateTime(slot.startTime).format(context);
+
+//                 return ListTile(
+//                   title: Text("Timing: $time"),
+//                   subtitle: Text("Day: $dayName  |  Date: $date"),
+//                   trailing: IconButton(
+//                     icon: const Icon(Icons.delete, color: Colors.red),
+//                     onPressed: () {
+//                       setState(() {
+//                         BookingStore().selectedSlots.remove(slot);
+//                       });
+//                     },
+//                   ),
+//                 );
+//               },
+//             ),
+//     );
+//   }
+// }
+import 'package:flutter/material.dart';
 import 'package:skintelligent/commons.dart';
 import 'package:skintelligent/models/available_booking_model.dart';
 import 'package:skintelligent/utiles/booking_store.dart';
@@ -14,8 +64,7 @@ class UserBookingScreen extends StatefulWidget {
 class _UserBookingScreenState extends State<UserBookingScreen> {
   @override
   Widget build(BuildContext context) {
-    final List<AppointmentSlot> bookedSlots =
-        BookingStore().selectedSlots.toList();
+    final List<Session> bookedSlots = BookingStore().selectedSlots.toList();
 
     return Scaffold(
       appBar: AppBar(title: const Text('My Bookings')),
@@ -25,10 +74,13 @@ class _UserBookingScreenState extends State<UserBookingScreen> {
               itemCount: bookedSlots.length,
               itemBuilder: (context, index) {
                 final slot = bookedSlots[index];
-                final dayName = DateFormat.EEEE().format(slot.startTime);
-                final date = slot.startTime.toLocal().toString().split(' ')[0];
-                final time =
-                    TimeOfDay.fromDateTime(slot.startTime).format(context);
+
+                // نحول ال startTime من String لـ DateTime
+                final startDateTime = DateTime.parse(slot.startTime);
+
+                final dayName = DateFormat.EEEE().format(startDateTime); // Sunday, Monday...
+                final date = DateFormat('yyyy-MM-dd').format(startDateTime); // 2025-04-23
+                final time = TimeOfDay.fromDateTime(startDateTime).format(context); // 6:00 PM
 
                 return ListTile(
                   title: Text("Timing: $time"),
