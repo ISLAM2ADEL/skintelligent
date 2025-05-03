5da9995ab5392779a3224345288d2356a2086764
= <tarekmohammed022@gmail.com>
2025-05-03 11:04:19 +0300

    change the name of api Key from status to statusCode

--- lib/controllers/error/error_model.dart
+++ lib/controllers/error/error_model.dart
@@ -1,11 +1,12 @@
-import 'package:skintelligent/commons.dart' ; 
+import 'package:skintelligent/commons.dart';
+
 class ErrorModel {
   final int status;
   final String errorMessage;
   ErrorModel({required this.status, required this.errorMessage});
   factory ErrorModel.fromJson(Map<String, dynamic> json) {
     return ErrorModel(
-      status: json[ApiKey.status],
+      status: json[ApiKey.statusCode],
       errorMessage: json[ApiKey.errorMessage],
     );
   }
